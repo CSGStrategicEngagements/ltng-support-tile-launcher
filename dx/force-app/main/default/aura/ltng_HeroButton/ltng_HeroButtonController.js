70780835349831124a7ba13a75fe6cff6f481409
Paul Roth <paulroth3d@gmail.com>
2018-09-26 23:09:41 -0500

    UPDATE: allow using a dynamic resource name

--- dx/force-app/main/default/aura/ltng_HeroButton/ltng_HeroButtonController.js
+++ dx/force-app/main/default/aura/ltng_HeroButton/ltng_HeroButtonController.js
@@ -1,6 +1,8 @@
 ({
 	init : function(component, event, helper) {
 		//console.log('hero button initialized');
+		var resourceName = component.get('v.resourceName');
+		helper.locateResource(component, helper, resourceName);
 	},
 
 	handleClick : function(component, event, helper){
