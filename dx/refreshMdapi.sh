9db2b93255bb0c1294372f50e1d180c41fb52001
Paul Roth <paulroth3d@gmail.com>
2018-10-15 08:10:44 -0500

    INTERNAL: include refreshMDAPI script

--- dx/refreshMdapi.sh
+++ dx/refreshMdapi.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-rm -rdf mdapiRefresh/
+# refresh the mdapi folder - for the latest package.xml file
+sfdx force:source:convert -r force-app -d ../mdapi
+# retrieve the latest using the package
 sfdx force:mdapi:retrieve -s -k ../mdapi/package.xml -r ../mdapiRefresh
+# unzip the package so we can import it
 sfdx dbs:zip:uncompress -s ../mdapiRefresh/unpackaged.zip -t ../mdapiRefresh/ -r
+# import the files
 sfdx force:mdapi:convert -r ../mdapiRefresh
 
